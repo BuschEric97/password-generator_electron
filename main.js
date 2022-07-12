a8a7697cd07ce4469f33359c535ce05ea35a2c34
Eric Busch <buscheric97@gmail.com>
2022-07-12 17:07:50 -0500

    Added bare-bones Electron app

---
+++ main.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+const loadMainWindow = () => {
+    const mainWindow = new BrowserWindow({
+        width : 1200,
+        height: 800,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    mainWindow.loadFile(path.join(__dirname, "index.html"));
+}
+
+// open main window on startup of app
+app.on("ready", loadMainWindow);
+
+// actually close the app when all windows are closed
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+      app.quit();
+    }
+});
+
+// open main window when app icon is clicked in OS's dock
+app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        loadMainWindow();
+    }
+});
