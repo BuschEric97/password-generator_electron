347b4fc1836150e00eacd0574f478e9d70ad89c3
Eric Busch <buscheric97@gmail.com>
2022-07-13 17:46:31 -0500

    Finished setting up structure of app

--- password-gen.js
+++ password-gen.js
@@ -4,9 +4,9 @@ const NOTIFICATION_NOT_IMPLEMENTED_TITLE = 'ERROR!';
 const NOTIFICATION_NOT_IMPLEMENTED_BODY = 'Feature not yet implemented!';
 
 function generate_password() {
-    document.getElementById('password-result').textContent = 'Test_Password';
+    document.getElementById('password-result').value = 'Test_Password';
 }
 
 function copy_to_clipboard() {
-    document.getElementById('password-result').textContent = 'Function not yet implemented!';
+    document.getElementById('password-result').value = 'Function not yet implemented!';
 }
